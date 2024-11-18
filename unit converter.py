a59047c36615628dfdebbc463d2a8e5ae7645b14
SnakeRuff <116602216+SnakeRuff@users.noreply.github.com>
2024-11-17 22:13:53 -0500

    Fleshed out program flow in the comments and created skeleton functions to be used

--- unit converter.py
+++ unit converter.py
@@ -1,5 +1,22 @@
 # Create a program that allows users to convert between different units. The user will:
 # Select a category (e.g., length, weight, temperature).
-# Input a value and select the units to convert from and to.
-# See the converted value displayed.
+# Input a value and select the units to convert from and to. See the converted value displayed.
+# Categories will include length, weight, and temperature, with conversions made between imperial & metric
+
+# Length: 1 meter = 39.37 inches | 1 cm = 0.3937 inches
+# Weight: 1 gram = 0.035 oz | 1 kg = 2.2 lbs
+# Temperature: Celsius -> Farenheit is Celsius * 9, divided by 5, then add 32
+
+# Program is broken into user input, GUI menu, conversion calc
+# Next progression is to provide user with ability to bulk upload via xlsx conversions to be calculated. Program will then return an xlsx including the finished calcs
+
+
+
+def userInput():
+
+def conversionCalc():
+
+
+while True:
+
 
