4d2a0a7110a44824988e14e326590fcea755ab76
SnakeRuff <116602216+SnakeRuff@users.noreply.github.com>
2024-11-17 21:50:19 -0500

    Creating new unit converter project files

---
+++ unit converter.py
@@ -0,0 +1,5 @@
+# Create a program that allows users to convert between different units. The user will:
+# Select a category (e.g., length, weight, temperature).
+# Input a value and select the units to convert from and to.
+# See the converted value displayed.
+
